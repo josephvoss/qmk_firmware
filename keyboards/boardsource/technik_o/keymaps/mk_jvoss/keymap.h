77a34b1a3be546a49d1217f41753687b5e522d3d
Joseph Voss <jvoss@josephvoss.com>
2022-08-14 10:48:52 -0500

    Tmp before rebase:

--- keyboards/boardsource/technik_o/keymaps/mk_jvoss/keymap.h
+++ keyboards/boardsource/technik_o/keymaps/mk_jvoss/keymap.h
@@ -15,10 +15,10 @@ K20,   K21,   K22,   K23,   K24,                 K25,   K26,   K27,   K28,   K29
 N30,   N31,   K32,   K33,   K34,                 K35,   K36,   K37,   N38,   N39\
 )\
 LAYOUT_ortho_4x12(\
-KC_NO, KC_NO, K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   KC_NO, KC_NO,\
-KC_NO, K00,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K09,   KC_NO,\
-KC_NO, K10,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K19,   KC_NO,\
-KC_NO, K20,   KC_NO, K32,   K33,   K34,   K35,   K36,   K37,   KC_NO, K29,   KC_NO \
+KC_NO, K00,   K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   KC_NO,\
+KC_NO, K10,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K19,   KC_NO,\
+KC_NO, K20,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K29,   KC_NO,\
+KC_NO, KC_NO, KC_NO, K32,   K33,   K34,   K35,   K36,   K37,   KC_NO, KC_NO, KC_NO \
 )
 
 /*
@@ -33,6 +33,12 @@ KC_NO, K01,   K02,   K03,   K04,   KC_NO, KC_NO, K05,   K06,   K07,   K08,   KC_
 K00,   K11,   K12,   K13,   K14,   KC_NO, KC_NO, K15,   K16,   K17,   K18,   K09,\
 K10,   K21,   K22,   K23,   K24,   KC_NO, KC_NO, K25,   K26,   K27,   K28,   K19,\
 K20,   KC_NO, KC_NO, K32,   K33,   K34,   K35,   K36,   K37,   KC_NO, KC_NO, K29\
+)
+LAYOUT_ortho_4x12(\
+KC_NO, KC_NO, K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   KC_NO, KC_NO,\
+KC_NO, K00,   K11,   K12,   K13,   K14,   K15,   K16,   K17,   K18,   K09,   KC_NO,\
+KC_NO, K10,   K21,   K22,   K23,   K24,   K25,   K26,   K27,   K28,   K19,   KC_NO,\
+KC_NO, K20,   KC_NO, K32,   K33,   K34,   K35,   K36,   K37,   KC_NO, K29,   KC_NO \
 )
 */
 
