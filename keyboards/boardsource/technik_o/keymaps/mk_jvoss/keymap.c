cbd73876041af49c793b35c1b19758e511298ebb
Joseph Voss <jvoss@josephvoss.com>
2024-03-26 11:07:42 -0500

    lets split changes

--- keyboards/boardsource/technik_o/keymaps/mk_jvoss/keymap.c
+++ keyboards/boardsource/technik_o/keymaps/mk_jvoss/keymap.c
@@ -9,11 +9,13 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_miryoku(
     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,
-    LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,   KC_H,   LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),   LGUI_T(KC_QUOT),
+    LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,   KC_H,   LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),   LGUI_T(KC_COLN),
     LT(BUTTON,   KC_Z),   ALGR_T(KC_X),   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,   ALGR_T(KC_DOT),   LT(BUTTON,   KC_SLSH),
-    U_NP,   U_NP,   LT(MEDIA,   KC_ESC),   LT(NAV,   KC_TAB),   LT(MOUSE,   KC_BSPC),   LT(SYM,   KC_SPC),   LT(NUM,   KC_ENT),   LT(FUN,   KC_DEL),   U_NP,   U_NP
+    U_NP,   U_NP,   LT(MEDIA,   KC_ESC),   LT(NAV,   KC_SPC),   LT(MOUSE,   KC_TAB),   LT(SYM,   KC_ENT),   LT(NUM,   KC_BSPC),   LT(FUN,   KC_DEL),   U_NP,   U_NP
   ),
-/*    U_NP,   U_NP,   LT(MEDIA,   KC_ESC),   LT(NAV,   KC_SPC),   LT(MOUSE,   KC_TAB),   LT(SYM,   KC_ENT),   LT(NUM,   KC_BSPC),   LT(FUN,   KC_DEL),   U_NP,   U_NP*/
+/*   Old home row w/ " in place of colon LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_D),   LSFT_T(KC_F),   KC_G,   KC_H,   LSFT_T(KC_J),   LCTL_T(KC_K),   LALT_T(KC_L),   LGUI_T(KC_QUOT),*/
+/*   My modified thumb row to match lets split U_NP,   U_NP,   LT(MEDIA,   KC_ESC),   LT(NAV,   KC_TAB),   LT(MOUSE,   KC_BSPC),   LT(SYM,   KC_SPC),   LT(NUM,   KC_ENT),   LT(FUN,   KC_DEL),   U_NP,   U_NP*/
+/*   Default mh row U_NP,   U_NP,   LT(MEDIA,   KC_ESC),   LT(NAV,   KC_SPC),   LT(MOUSE,   KC_TAB),   LT(SYM,   KC_ENT),   LT(NUM,   KC_BSPC),   LT(FUN,   KC_DEL),   U_NP,   U_NP*/
   [NAV] = LAYOUT_miryoku(
     RESET,   U_NA,    U_NA,    U_NA,    U_NA,    U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS,
