cb01e350be3b2eadf9a0d05c8bca398f1f7a86e1
Joseph Voss <jvoss@josephvoss.com>
2024-03-26 11:31:26 -0500

    Add mk layout to lets split

---
+++ keyboards/lets_split/rev2/rev2.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include "lets_split.h"
+
+#include "quantum.h"
+
+#ifdef USE_I2C
+#include <stddef.h>
+#ifdef __AVR__
+	#include <avr/io.h>
+	#include <avr/interrupt.h>
+#endif
+#endif
+
+#ifndef FLIP_HALF
+// Standard Keymap
+// (TRRS jack on the left half is to the right, TRRS jack on the right half is to the left)
+#define LAYOUT( \
+	L00, L01, L02, L03, L04, L05, R00, R01, R02, R03, R04, R05, \
+	L10, L11, L12, L13, L14, L15, R10, R11, R12, R13, R14, R15, \
+	L20, L21, L22, L23, L24, L25, R20, R21, R22, R23, R24, R25, \
+	L30, L31, L32, L33, L34, L35, R30, R31, R32, R33, R34, R35 \
+	) \
+	{ \
+		{ L00, L01, L02, L03, L04, L05 }, \
+		{ L10, L11, L12, L13, L14, L15 }, \
+		{ L20, L21, L22, L23, L24, L25 }, \
+		{ L30, L31, L32, L33, L34, L35 }, \
+		{ R05, R04, R03, R02, R01, R00 }, \
+		{ R15, R14, R13, R12, R11, R10 }, \
+		{ R25, R24, R23, R22, R21, R20 }, \
+		{ R35, R34, R33, R32, R31, R30 } \
+	}
+#else
+// Keymap with right side flipped
+// (TRRS jack on both halves are to the right)
+#define LAYOUT( \
+	L00, L01, L02, L03, L04, L05, R00, R01, R02, R03, R04, R05, \
+	L10, L11, L12, L13, L14, L15, R10, R11, R12, R13, R14, R15, \
+	L20, L21, L22, L23, L24, L25, R20, R21, R22, R23, R24, R25, \
+	L30, L31, L32, L33, L34, L35, R30, R31, R32, R33, R34, R35 \
+	) \
+	{ \
+		{ L00, L01, L02, L03, L04, L05 }, \
+		{ L10, L11, L12, L13, L14, L15 }, \
+		{ L20, L21, L22, L23, L24, L25 }, \
+		{ L30, L31, L32, L33, L34, L35 }, \
+		{ R00, R01, R02, R03, R04, R05 }, \
+		{ R10, R11, R12, R13, R14, R15 }, \
+		{ R20, R21, R22, R23, R24, R25 }, \
+		{ R30, R31, R32, R33, R34, R35 } \
+	}
+#endif
+
+#define LAYOUT_ortho_4x12 LAYOUT
+
+#define LAYOUT_miryoku(\
+	L00, L01, L02, L03, L04, R00, R01, R02, R03, R04, \
+	L10, L11, L12, L13, L14, R10, R11, R12, R13, R14, \
+	L20, L21, L22, L23, L24, R20, R21, R22, R23, R24, \
+	L30, L31, L32, L33, L34, R30, R31, R32, R33, R34 \
+	) \
+	{ \
+		{ KC_NO, L00, L01, L02, L03, L04 }, \
+		{ KC_NO, L10, L11, L12, L13, L14 }, \
+		{ KC_NO, L20, L21, L22, L23, L24 }, \
+		{ KC_NO, L30, L31, L32, L33, L34 }, \
+		{ KC_NO, R04, R03, R02, R01, R00 }, \
+		{ KC_NO, R14, R13, R12, R11, R10 }, \
+		{ KC_NO, R24, R23, R22, R21, R20 }, \
+		{ KC_NO, R34, R33, R32, R31, R30 } \
+	}
+
+#define U_NP KC_NO // key is not present
+#define U_NA KC_NO // present but not available for use
+#define U_NU KC_NO // available but not used
+
+#if defined MIRYOKU_CLIPBOARD_FUN
+#define U_RDO KC_AGIN
+#define U_PST KC_PSTE
+#define U_CPY KC_COPY
+#define U_CUT KC_CUT
+#define U_UND KC_UNDO
+#elif defined MIRYOKU_CLIPBOARD_MAC
+#define U_RDO SCMD(KC_Z)
+#define U_PST LCMD(KC_V)
+#define U_CPY LCMD(KC_C)
+#define U_CUT LCMD(KC_X)
+#define U_UND LCMD(KC_Z)
+#elif defined MIRYOKU_CLIPBOARD_WIN
+#define U_RDO C(KC_Y)
+#define U_PST C(KC_V)
+#define U_CPY C(KC_C)
+#define U_CUT C(KC_X)
+#define U_UND C(KC_Z)
+#else
+#define U_RDO KC_AGIN
+#define U_PST S(KC_INS)
+#define U_CPY C(KC_INS)
+#define U_CUT S(KC_DEL)
+#define U_UND KC_UNDO
+#endif
