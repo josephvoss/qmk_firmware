008db42f8e231318cb5500c9d989062b7d2004af
Joseph Voss <jvoss@josephvoss.com>
2024-04-25 11:05:46 -0500

    Fix ordering of home row modifiers
    Vim macros didn't solve everything :/

--- keyboards/hazel/bad_wings_v2/keymaps/jvoss/keymap.c
+++ keyboards/hazel/bad_wings_v2/keymaps/jvoss/keymap.c
@@ -5,7 +5,7 @@ enum layers { BASE, MEDIA, NAV, MOUSE, SYMB, NUM, FUNC };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT_split_3x5_3(
     KC_Q             ,  KC_W            ,  KC_E             ,  KC_R            ,  KC_T           ,  KC_Y           ,  KC_U         ,  KC_I         ,  KC_O         ,  KC_P            ,
-    LGUI_T(KC_A)     ,  LALT_T(KC_S)    ,  LCTL_T(KC_D)     ,  LSFT_T(KC_F)    ,  KC_G           ,  KC_H           ,  LGUI_T(KC_J) ,  LALT_T(KC_K) ,  LCTL_T(KC_L) ,  LSFT_T(KC_QUOT) ,
+    LGUI_T(KC_A)     ,  LALT_T(KC_S)    ,  LCTL_T(KC_D)     ,  LSFT_T(KC_F)    ,  KC_G           ,  KC_H           ,  LSFT_T(KC_J) ,  LCTL_T(KC_K) ,  LALT_T(KC_L) ,  LGUI_T(KC_QUOT) ,
     KC_Z             ,  KC_X            ,  KC_C             ,  KC_V            ,  KC_B           ,  KC_N           ,  KC_M         ,  KC_COMM      ,  KC_DOT       ,  KC_SLSH         ,
     LT(MEDIA,KC_ESC) ,  LT(NAV,KC_BSPC) ,  LT(MOUSE,KC_TAB) ,  LT(SYMB,KC_ENT) ,  LT(NUM,KC_SPC) ,  LT(FUNC,KC_DEL)
   ),
