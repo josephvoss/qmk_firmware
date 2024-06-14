d000366266d95e360243e12fc376e871dfa98537
Joseph Voss <jvoss@josephvoss.com>
2024-06-14 15:35:15 -0500

    Fix mouse layers

--- keyboards/hazel/bad_wings_v2/keymaps/jvoss/keymap.c
+++ keyboards/hazel/bad_wings_v2/keymaps/jvoss/keymap.c
@@ -1,13 +1,21 @@
 #include QMK_KEYBOARD_H
 
-enum layers { BASE, MEDIA, NAV, MOUSE, SYMB, NUM, FUNC };
+enum layers {
+  BASE,
+  MEDIA,
+  NAV,
+  MOUSE,
+  SYMB,
+  NUM,
+  FUNC
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT_split_3x5_3(
     KC_Q             ,  KC_W            ,  KC_E             ,  KC_R            ,  KC_T           ,  KC_Y           ,  KC_U         ,  KC_I         ,  KC_O         ,  KC_P            ,
     LGUI_T(KC_A)     ,  LALT_T(KC_S)    ,  LCTL_T(KC_D)     ,  LSFT_T(KC_F)    ,  KC_G           ,  KC_H           ,  LSFT_T(KC_J) ,  LCTL_T(KC_K) ,  LALT_T(KC_L) ,  LGUI_T(KC_QUOT) ,
     KC_Z             ,  KC_X            ,  KC_C             ,  KC_V            ,  KC_B           ,  KC_N           ,  KC_M         ,  KC_COMM      ,  KC_DOT       ,  KC_SLSH         ,
-    LT(MEDIA,KC_ESC) ,  LT(NAV,KC_BSPC) ,  LT(MOUSE,KC_TAB) ,  LT(SYMB,KC_ENT) ,  LT(NUM,KC_SPC) ,  LT(FUNC,KC_DEL)
+    LT(MEDIA,KC_ESC) ,  LT(NAV,KC_BSPC)  ,  LT(MOUSE,KC_TAB),  LT(SYMB,KC_ENT) ,  LT(NUM,KC_SPC) ,  LT(FUNC,KC_DEL)
   ),
 	[MEDIA] = LAYOUT_split_3x5_3(
     KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , RGB_TOG , RGB_MOD , RGB_HUI , RGB_SAI , RGB_VAI ,
@@ -25,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO   ,  KC_NO   ,  KC_NO   ,  KC_NO   ,  KC_NO   ,  KC_AGIN ,  KC_PSTE ,  KC_COPY ,  KC_CUT  ,  KC_UNDO,
     KC_LGUI ,  KC_LALT ,  KC_LCTL ,  KC_LSFT ,  KC_NO   ,  KC_MS_L ,  KC_MS_D ,  KC_MS_U ,  KC_MS_R ,  KC_NO,
     KC_NO   ,  KC_ALGR ,  KC_NO   ,  KC_NO   ,  KC_NO   ,  KC_WH_L ,  KC_WH_D ,  KC_WH_U ,  KC_WH_R ,  KC_NO,
-    KC_NO   ,  KC_NO   ,  KC_NO   ,  KC_BTN1 ,  KC_BTN3 ,  KC_BTN2
+    KC_NO   ,  KC_NO   ,  KC_NO   ,  KC_BTN2 ,  KC_BTN1 ,  KC_BTN3
   ),
 	[SYMB] = LAYOUT_split_3x5_3(
     KC_LCBR ,  KC_AMPR ,  KC_ASTR ,  KC_LPRN ,  KC_RCBR ,  KC_NO ,  KC_NO   ,  KC_NO   ,  KC_NO   ,  KC_NO,
@@ -46,13 +54,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_APP ,  KC_SPC ,  KC_TAB ,  KC_NO ,  KC_NO   ,  KC_NO
   )
 };
-
-#if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-
-};
-#endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
-
-
-
-
